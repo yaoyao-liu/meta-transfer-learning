2209a94e0c5b737bf47efa94047c80f241ca6369
Yaoyao Liu <lyy@outlook.sg>
2019-04-01 16:54:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## Created by: Yaoyao Liu
 ## NUS School of Computing
-## yaoyaoliu@u.nus.edu
+## yaoyao.liu@u.nus.edu
 ## Copyright (c) 2019
 ##
 ## This source code is licensed under the MIT-style license found in the
