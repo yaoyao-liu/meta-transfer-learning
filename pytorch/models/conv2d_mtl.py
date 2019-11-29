769733a8c9495c4d7672ee4fa48b6e72abc38e0d
Yaoyao Liu <yaoyaoliu@outlook.com>
2019-11-29 10:11:35 +0800

    Update conv2d_mtl.py

--- pytorch/models/conv2d_mtl.py
+++ pytorch/models/conv2d_mtl.py
@@ -14,7 +14,7 @@ import torch
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
-from torch.nn.modules.utils import _single, _pair, _triple
+from torch.nn.modules.utils import _pair
 
 class _ConvNdMtl(Module):
     """The class for meta-transfer convolution"""
